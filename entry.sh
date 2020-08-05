832e0f9e295efe857da702edb7476d5d91a06f17
MrChenWithCapsule <Mr.ChenWithCapsule@outlook.com>
2020-08-05 21:04:43 +0800

    add default env value

--- entry.sh
+++ entry.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 set -e
 
+set_env() {
+    local name="$1"
+    local default="$2"
+    if [[ -z "${$name}" ]]
+    then
+        export "$name"="$default"
+    fi
+}
+
+set_env "RELAY_HOST" "opensmtpd"
+set_env "RELAY_PORT" "24"
+
 socat "tcp-listen:$RELAY_PORT" "tcp-connect:$RELAY_HOST"":$RELAY_PORT" &
 exec dkimproxy.out --conf_file=/etc/dkimproxy/dkimproxy_out.conf
