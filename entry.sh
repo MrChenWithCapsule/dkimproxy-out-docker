78c480b9ffcc36b5f41d6d80f4e61724df6f11f8
MrChenWithCapsule <Mr.ChenWithCapsule@outlook.com>
2020-08-06 21:21:35 +0800

    fix socat exiting

--- entry.sh
+++ entry.sh
@@ -8,5 +8,5 @@ if [[ -z "$RELAY_PORT" ]]; then
     RELAY_PORT="24"
 fi
 
-socat "tcp-listen:$RELAY_PORT" "tcp-connect:$RELAY_HOST"":$RELAY_PORT" &
+socat "tcp-listen:$RELAY_PORT",fork "tcp-connect:$RELAY_HOST"":$RELAY_PORT" &
 exec dkimproxy.out --conf_file=/etc/dkimproxy/dkimproxy_out.conf --group=dkimproxy --user=dkimproxy
